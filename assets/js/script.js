f57726af0060b076748f2ffc951be690429b542c
remishn <nsarem65@gmail.com>
2022-05-29 15:18:54 -0500

    preventDefalul added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,13 @@
-var buttonE1 = document.querySelector("#save-task")
+var formEl = document.querySelector("#task-form"); 
+var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
-var tasksToDoEl = document.querySelector("#tasks-to-do");
+var createTaskHandler = function(event) { 
+  event.preventDefault(); 
 
-buttonE1.addEventListener("click", function() {
-  var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
-  tasksToDoEl.appendChild(listItemEl);
-});
+  var listItemEl = document.createElement("li"); 
+  listItemEl.className = "task-item"; 
+  listItemEl.textContent = "This is a new task."; 
+  tasksToDoEl.appendChild(listItemEl); 
+  }; 
 
-var createTaskHandler = function() {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-  }
-buttonE1.addEventListener("click", createTaskHandler);
+  formEl.addEventListener("submit", createTaskHandler);
